717ba30b82f209be1527b0e82d860946dfa1b47a
Murasamy <peiyang7@illinois.edu>
2025-02-08 17:26:50 -0600

    Update 14.s_road_shake_camera.py

--- example/14.s_road_shake_camera.py
+++ example/14.s_road_shake_camera.py
@@ -8,9 +8,7 @@ DangerDistance = 25 # > 20 && < 40 turn around,
 def main():
     try:
         px = Picarx()
-        # px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo
-        music.music_play('../musics/slow-trail-Ahjay_Stelino.mp3')
-       
+        # px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo       
         while True:
             distance = round(px.ultrasonic.read(), 2)
             print("distance: ",distance)
