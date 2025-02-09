f46bfa2fe09a7f5d80bd4d4e7951de0249cf280f
Murasamy <peiyang7@illinois.edu>
2025-02-08 18:32:12 -0600

    music update

--- example/15.shake_with_music.py
+++ example/15.shake_with_music.py
@@ -31,7 +31,7 @@ def play_music():
             flag_bgm = not flag_bgm
             if flag_bgm is True:
                 print('Play Music')
-                music.music_play('../musics/Chopin-Nocturne no 20.mp3')
+                music.music_play('../musics/Chopin-Nocturne.mp3')
             else:
                 print('Stop Music')
                 music.music_stop()
