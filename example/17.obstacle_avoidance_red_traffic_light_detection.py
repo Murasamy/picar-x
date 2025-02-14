156b1da443f4ac520ea155d028c813da46270ab7
Murasamy <peiyang7@illinois.edu>
2025-02-14 15:02:15 -0600

    red light detection

--- example/17.obstacle_avoidance_red_traffic_light_detection.py
+++ example/17.obstacle_avoidance_red_traffic_light_detection.py
@@ -22,47 +22,39 @@ Vilib.traffic_sign_obj_parameter['acc']     # accuracy
   
 '''
 
+
+
 def main():
     try:
         px = Picarx()
         # px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo
-        Vilib.camera_start(vflip=False, hflip=False) # , size=(640, 480)
+        Vilib.camera_start(vflip=False, hflip=False) # size=(640,, 480)
         Vilib.show_fps()
         Vilib.display(local=True, web=True)
-        Vilib.traffic_detect_switch(True)
+        Vilib.color_detect(color="red")  # red, green, blue, yellow , orange, purple
         sleep(1)
+
+        last_camera_detection_time = time.time()
+        camera_detection_interval = 1  # seconds
+
        
         while True:
             distance = round(px.ultrasonic.read(), 2)
-            print("distance: ",distance)
-            t = Vilib.traffic_sign_obj_parameter['t']
+            print("distance: ", distance)
             
-            if t != 'none': # traffic sign detected
-                x = Vilib.traffic_sign_obj_parameter['x']
-                y = Vilib.traffic_sign_obj_parameter['y']
-                w = Vilib.traffic_sign_obj_parameter['w']
-                h = Vilib.traffic_sign_obj_parameter['h']
-                acc = Vilib.traffic_sign_obj_parameter['acc']
-                print(f"traffic sign detected: {t}, acc: {acc}, x: {x}, y: {y}, w: {w}, h: {h}")
-                if t == 'stop':
+            current_time = time.time()
+            if current_time - last_camera_detection_time >= camera_detection_interval:
+                last_camera_detection_time = current_time
+                n = Vilib.color_obj_parameter['n']
+                color = Vilib.color_obj_parameter['color']
+                
+                if n != 0: # traffic sign detected
+                    x = Vilib.color_obj_parameter['x']
+                    y = Vilib.color_obj_parameter['y']
+                    w = Vilib.color_obj_parameter['w']
+                    h = Vilib.color_obj_parameter['h']
+                    print(f"stop traffic sign detected, x: {x}, y: {y}, w: {w}, h: {h}")
                     px.stop()
-                    print("stop")
-                    sleep(2)
-                elif t == 'right':
-                    px.set_dir_servo_angle(30)
-                    px.forward(POWER)
-                    print("turn right")
-                    sleep(2)
-                elif t == 'left':
-                    px.set_dir_servo_angle(-30)
-                    px.forward(POWER)
-                    print("turn left")
-                    sleep(2)
-                elif t == 'forward':
-                    px.set_dir_servo_angle(0)
-                    px.forward(POWER)
-                    print("go forward")
-                    sleep(2)
 
             if distance >= SafeDistance:
                 px.set_dir_servo_angle(0)
@@ -75,7 +67,6 @@ def main():
                 px.set_dir_servo_angle(-30)
                 px.backward(POWER)
                 time.sleep(0.8)
-
     finally:
         px.forward(0)
 
